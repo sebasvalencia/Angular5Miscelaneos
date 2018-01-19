718812c7839483c7662e6e34f6ea81f1860c0cae
Sebastian Valencia <sebastian.valencian@gmail.com>
2018-01-18 19:05:36 -0500

    Quinta app en Angular - Miscelaneos - Se crean las rutas hijas en el app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { HomeComponent } from './components/home/home.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
+import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo.component';
+import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
+import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle.component';
 
-
+//usuario/10/nuevo
 
 const routes: Routes = [
     { path: 'home', component: HomeComponent },
-    { path: 'usuario', component: UsuarioComponent },
+    { 
+        path: 'usuario/:id',
+        component: UsuarioComponent,
+        children: [
+            {
+                path:'nuevo', component: UsuarioNuevoComponent
+            },
+            {
+                path:'editar', component: UsuarioEditarComponent
+            },
+            {
+                path:'detalle', component: UsuarioDetalleComponent
+            },
+            { path: '**', pathMatch:'full', redirectTo:'nuevo' }
+        ]
+     },
     { path: '**', pathMatch:'full', redirectTo:'home' }
 ];
 
